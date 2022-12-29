16d62282aed36c65bbbeb305fd16c834ecba7709
jay <jaypifiverr@gmail.com>
2022-12-29 07:40:29 -0500

    commit tree c58b8442f39e1ac92814bc2b18b9f76ff2eda843

--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,11 @@
 require 'bundler/setup'
 Bundler.require
 
+desc 'drop into the Pry console'
+task console: :environment do
+  Pry.start
+end
+
 require_relative "../lib/student"
 
 DB = { conn: SQLite3::Database.new("db/students.db") }
